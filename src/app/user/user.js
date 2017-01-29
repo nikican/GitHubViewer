ff5dfe495d4596a42e31af5b2e8767c58b70031b
Nikica Novakovic <novakovicnikica@gmail.com>
2017-01-29 18:43:23 +0100

    Upate user page

--- src/app/user/user.js
+++ src/app/user/user.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 	'use-strict';
 
 	angular
@@ -8,9 +8,18 @@
 	function UserController(github, $routeParams) {
 		var vm = this;
 		vm.username = $routeParams.username;
-		vm.repositoriesSortOrder = "-stargazers_count";
+		vm.sortField = 'name'; // set the default sort field
+		vm.sortReverse = false;  // set the default sort order
+		vm.searchRepo = '';
+		vm.sortByName = sortByName;
+		vm.sortByStargazersCount = sortByStargazersCount;
+		vm.sortByLanguage = sortByLanguage;
 
-		searchForUser();
+		activate();
+
+		function activate() {
+			searchForUser();
+		}
 
 		function searchForUser() {
 			github.getUser(vm.username).then(onSearchForUserSuccess, onSearchForUserError);
@@ -38,5 +47,21 @@
 		function onLoadRepositoriesError(response) {
 			vm.error = 'Could not fetch repositories';
 		};
+
+		function sortByName() {
+			console.log('sort by name');
+			vm.sortField = 'name';
+			vm.sortReverse = !vm.sortReverse
+		}
+		function sortByStargazersCount() {
+			console.log('sort by stargazers_count');
+			vm.sortField = 'stargazers_count';
+			vm.sortReverse = !vm.sortReverse;
+		}
+		function sortByLanguage() {
+			console.log('sortByLanguage');
+			vm.sortField = 'language';
+			vm.sortReverse = !vm.sortReverse;
+		}
 	}
-}());
+} ());
